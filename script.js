c8c70792720d78455da70c25b8a1245d875fa79d
itssTim <86143692+itssTim@users.noreply.github.com>
2026-01-06 18:37:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ function getData(form) {
         console.log(pair[0] + ":" + pair[1])}
     }*/
     let dataObj = Object.fromEntries(formData);
-    let uuid = self.crypto.randomUUID();
-    dataObj.custId = uuid;
+    dataObj.custId = crypto.randomUUID();
     return dataObj;
 }
 
 //Add eventListener for form submittal and prevent default of page reload
 document.getElementById("contactForm").addEventListener("submit", function(e) {
     e.preventDefault();
-getData(e.target);
+
+const form = e.target;
+const dataObj = getData(form);
 
 //Ensures dataLayer exists and push to GTM
 window.dataLayer = window.dataLayer || [];
